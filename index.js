225cbb8c487cd1201e6de4db3c49eebb0a96710e
Christian Carrillo <216953130+77Oakz-C@users.noreply.github.com>
2025-07-16 14:57:52 -0700

    added js file

---
+++ index.js
@@ -0,0 +1,4 @@
+ img.addEventListener("click", () => {
+        img.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zinnedproject.org%2Fmaterials%2Fsimulating-redlining%2F&psig=AOvVaw3mwic01VCwTvvXu6a2zkcz&ust=1752788179132000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLDr0aCrwo4DFQAAAAAdAAAAABAE"; // 
+    });
+<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
