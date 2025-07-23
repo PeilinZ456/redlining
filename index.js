bccdb7ca7093bab7ccdc39afc0e65675c76c1761
Christian Carrillo <216953130+77Oakz-C@users.noreply.github.com>
2025-07-23 14:31:30 -0700

    fixed issues

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let mybutton = document.getElemByld("myBtn");
 window.onscroll = function(){scrollFunction()};
-if (document.body.scrollTop > 20 || document.documentElement.scrollTo p > 20) {mybutton.style.display = "block";} else
-{mybutton.style.display = "none";}
-function topFunction() {document.body.scrollTop = 0; document.documentElement.scrollTo p =0;}
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {mybutton.style.display = "block";} else{ mybutton.style.display = "none";}
+function topFunction() {document.body.scrollTop = 0; document.documentElement.scrollTop =0;}
 
 
 window.alert('This is a redlining page');
