d62c0b44ab4b2f9cee0920d3ee1b768d64d61220
Peilin Zhang <183445730+PeilinZ456@users.noreply.github.com>
2025-07-23 14:22:52 -0700

    new changes

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
- img.addEventListener("click", () => {
-        img.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zinnedproject.org%2Fmaterials%2Fsimulating-redlining%2F&psig=AOvVaw3mwic01VCwTvvXu6a2zkcz&ust=1752788179132000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLDr0aCrwo4DFQAAAAAdAAAAABAE"; // 
-    });
-<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+let mybutton = document.getElemByld("myBtn");
+window.onscroll = function(){scrollFunction()};
+if (document.body.scrollTop > 20 || document.documentElement.scrollTo p > 20) {mybutton.style.display = "block";} else
+
+window.alert('This is a redlining page');
+
+
+
